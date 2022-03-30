bb4aea83dd4db36c1dd4c235e9ed2e8e0340543f
kiquino <kiquino@gmail.com>
2022-03-30 13:40:02 -0300

    finalizando

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(function () {
             let dia = new Date(date).getDate();
             let mes = new Date(date).getMonth() + 1;
             let año = new Date(date).getFullYear();
-            $('#date').val(dia + "-" + mes + "-" + año);
+            $('#date').val(dia + "/" + mes + "/" + año);
 
         },
 
