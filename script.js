7e5a9ff9cefd2a967b0f07f632ac0676f2cf35ab
kiquino <kiquino@gmail.com>
2022-03-30 13:34:29 -0300

    avance

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.calendar-container').calendar({
+        date: new Date(),
+
+
+        onClickDate: function (date) {
+
+            let dia = new Date(date).getDate();
+            let mes = new Date(date).getMonth() + 1;
+            let año = new Date(date).getFullYear();
+            $('#date').val(dia + "-" + mes + "-" + año);
+
+        },
+
+        highlightSelectedWeekday: true,
+
+
+    });
+
+})
